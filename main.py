979bae3a67a385aa36acb59bf5f2e70e5bf67a3f
Bidulusha <74060730+Bidulusha@users.noreply.github.com>
2025-09-27 16:39:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 a,b = map(int, input().split())
 
+c = b
+
 d = {}
 mas = set()
-for i in range(b):
+for i in range(c):
     x, y = map(int, input().split())
+    if x == y:
+        b = 0
     mas.add(x)
     if x in d:
         d[x].append(y)
     else:
         d[x] = [y]
-c = mas.pop()
-a = d[c]
-for i in d:
-    if d[i] != a:
-        print(-1)
-        break
-else:
-    mas.add(c)
-    if len(mas) != len(d[c]):
-        print(-1)
+
+if b != 0:
+    c = mas.pop()
+    a = d[c]
+    for i in d:
+        if d[i] != a:
+            print(-1)
+            break
     else:
-        print(*sorted(mas))
+        mas.add(c)
+        if len(mas) != len(d[c]):
+            print(-1)
+        else:
+            print(*sorted(mas))
+else:
+    print(-1)
