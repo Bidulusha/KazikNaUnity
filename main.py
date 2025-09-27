8a6408263e8a4d991436abe605259b1d9f90eb3a
Bidulusha <74060730+Bidulusha@users.noreply.github.com>
2025-09-27 13:48:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-base = [1,6,4,2,7,5,3]
-s = int(input())
+d = {32: 36, 36: 37, 37: 38, 42:39, 43: 40, 45: 41, 46:42, 47:43, 58:44}
 
-for i in range(0, 70):
-    s = i
-    if (base[(s // 7) % 7] + s - (s // 7) * 7 - 1) % 7 == 0:
-        print(7)
-    else:
-        print((base[(s // 7) % 7] + s - (s // 7) * 7 - 1) % 7)
+s = input()
+mas = []
+
+def trans(a):
+    global d
+    if 47 < ord(a) and ord(a) < 58:
+        return(ord(a) - 48)
+
+    if 64 < ord(a) and ord(a) < 91:
+        return(ord(a) - 55)
+        
+    return (d[ord(a)])
+
+
+for i in range(1, len(s), +2):
+    mas.append('{0:011b}'.format(trans(s[i - 1].upper()) * 45 + trans(s[i].upper())))
+
+if len(s) % 2 != 0:
+    mas.append('{0:06b}'.format(trans(s[-1].upper())))
+
+
+print(*mas,sep='')
