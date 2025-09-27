ff4b9175509b8ffadf407f3d4b36a0bf41a5b3bc
Bidulusha <74060730+Bidulusha@users.noreply.github.com>
2025-09-27 14:59:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,32 +4,50 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     string a, b;
     cin >> a >> b;
     
-    int pattern[128] = {0};
-    for (char c : b) {
-        pattern[c]++;
+    int n = a.size(), m = b.size();
+    if (m > n) {
+        cout << "0\n";
+        return 0;
     }
     
-    for (size_t i = 0; i < a.length() - b.length() + 1; i++) {
-        bool found = true;
+    int pattern[128] = {0};
+    for (char c : b) pattern[c]++;
+    
+    int window[128] = {0};
+    int bad_chars = 0;
+    
+    // Считаем плохие символы в первом окне
+    for (int i = 0; i < m; i++) {
+        unsigned char c = a[i];
+        if (window[c]++ >= pattern[c]) bad_chars++;
+    }
+    
+    if (bad_chars == 0) {
+        cout << "1\n";
+        return 0;
+    }
+    
+    for (int i = m; i < n; i++) {
+        // Удаляем левый символ
+        unsigned char left = a[i - m];
+        if (window[left]-- > pattern[left]) bad_chars--;
         
-        int temp[128] = {0};
-        for (size_t j = 0; j < b.length(); j++) {
-            char c = a[i + j];
-            if (++temp[c] > pattern[c]) {
-                found = false;
-                break;
-            }
-        }
+        // Добавляем правый символ
+        unsigned char right = a[i];
+        if (window[right]++ >= pattern[right]) bad_chars++;
         
-        if (found) {
-            cout << i + 1 << endl;
+        if (bad_chars == 0) {
+            cout << i - m + 2 << '\n';
             return 0;
         }
     }
     
-    cout << 0 << endl;
+    cout << "0\n";
     return 0;
 }
